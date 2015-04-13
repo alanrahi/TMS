befedcddb05b28b4259a0562c7f990e1ecd9ba0b
Tal Zivelin <talzivelin@Tals-MacBook-Pro-2.local>
2015-04-12 23:39:15 -0700

    backbone portion of project

---
+++ js/models/task_model.js
@@ -0,0 +1,23 @@
+
+"use strict";
+APP.TaskModel = Backbone.Model.extend({
+  // you can set any defaults you would like here
+  defaults: {
+    title: "",
+    instance: "",
+    duration: ""
+  },
+
+  validate: function (attrs) {
+    var errors = {};
+    if (!attrs.title) errors.title = "Please Add Task Title";
+    if (!_.isEmpty(errors)) {
+      return errors;
+    }
+  }
+});
+
+APP.TaskCollection = Backbone.Collection.extend({
+  // Reference to this collection's model.
+  model: APP.TaskModel
+});
