2d5c4c7e8a6115d9af489e7dfe0a8a60cbfb8a58
tredmond <tredmond@urbangalaxie.com>
2015-04-21 21:06:44 -0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-dbKey: "30fcba2a-1c9e-496f-a138-79be519c8276"
-}
-
-/*curl -u '30fcba2a-1c9e-496f-a138-79be519c8276' https://api.orchestrate.io/v0/test-table
-
-
-
-curl -u '30fcba2a-1c9e-496f-a138-79be519c8276' -XPUT -H 'Content-Type:application/json' -d '{"test":"object"}' https://api.orchestrate.io/v0/test-table/special*/
