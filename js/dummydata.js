1e83e76eca6f9640d68aa5a2a8448fe470c48229
Alan Rahi <alan@webfordoctors.com>
2015-04-21 19:32:20 -0700

    added first dummy data file

---
+++ js/dummydata.js
@@ -0,0 +1,97 @@
+function database({
+
+    { "userID" : { "user" : { "firstname" : "alan",
+							     "lastname" : "rahi",
+							     "username" : "alanrahi",
+							     "location" : {"city : portland" , "state" : "OR"}
+							     "password" : {"salt" : 7 ,
+											   "hash" : "oreughawg8w30wfu0w9fuf3"}
+
+							},
+				  
+				   "day1" :	{			   
+									   "task1" : { "taskname" : "cigarettes",
+									   				"instances" : 4,
+									   				"duration" : 5
+												  },
+
+									   "task2" : { "taskname" : "blunts",
+									   				"instances" : 4,
+									   				"duration" : 10
+												  },
+
+									   "task3" : { "taskname" : "martinis",
+									   				"instances" : 5,
+									   				"duration" : 10
+												  }	
+							},
+							
+					"day2" :	{			   
+									   "task1" : { "taskname" : "cigarettes",
+									   				"instances" : 4,
+									   				"duration" : 10
+												  },
+
+									   "task2" : { "taskname" : "blunts",
+									   				"instances" : 4,
+									   				"duration" : 10
+												  },
+
+									   "task3" : { "taskname" : "martinis",
+									   				"instances" : 4,
+									   				"duration" : 10
+												  }	
+							},
+							
+					"day3" :	{			   
+									   "task1" : { "taskname" : "cigarette",
+									   				"instances" : 4,
+									   				"duration" : 5
+												  },
+
+									   "task2" : { "taskname" : "blunts",
+									   				"instances" : 4,
+									   				"duration" : 10
+												  },
+
+									   "task3" : { "taskname" : "martinis",
+									   				"instances" : 4,
+									   				"duration" : 10
+												  }	
+							},																	  			    		  	    
+
+/*values associated with a user: firstname, lastname, username,
+password, location */
+
+
+
+
+
+	}) {};
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
