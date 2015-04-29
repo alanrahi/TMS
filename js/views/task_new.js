8bedde9e13662737ec6e1afb3a46a0c40a11d4b6
Alan Rahi <alan@webfordoctors.com>
2015-04-28 20:19:49 -0700

    GET route and Post route are working better PUT is not quite working yet

--- js/views/task_new.js
+++ js/views/task_new.js
@@ -29,20 +29,33 @@ APP.TaskNewView = Backbone.View.extend({
     // update our model with values from the form
     console.log('console logging this.task');
     console.log(this.task);
+    var date = new Date();
+    var month = (date.getMonth()+1).toString();
+    var day = date.getDate().toString();
+    var year = date.getFullYear().toString();
+    var currentDate = month+"-"+day+"-"+year;
+    //console.log(currentDate);
+    //var currentDate = toString(date.getMonth())+"-"+toString(date.getDate())+"-"+toString(date.getFullYear());
+    console.log(currentDate);
     this.task.set({
       title: this.$el.find('input[name=title]').val(),
       duration: this.$el.find('input[name=duration]').val(),
+      date: currentDate
       //instance: this.$el.find('input[name=instance]').val(),
       // just setting random number for id would set as primary key from server
-      id: Math.floor(Math.random() * 100) + 1
+      //id: Math.floor(Math.random() * 100) + 1
     });
     if (this.task.isValid()){
       console.log('model is valid, adding to collection');
       // add it to the collection
       this.tasks.add(this.task);
-      // this.task.save();
+      this.task.save();
       // redirect back to the index
-      console.log(this);
+      console.log('logging result of isValid after hitting save button');
+      console.log(this); 
+     
+      
+      
       window.location.hash = "tasks/index";
       var divCountForWidthChanger = divCountForWidthChanger++;
     }
